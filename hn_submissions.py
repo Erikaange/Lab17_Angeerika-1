738908cc5e5cb5033269d17aab63a15ed682abbc
Erikaange <akengneheukoua@student.cscc.edu>
2025-05-10 17:13:42 -0400

    handled error by refactoring response_dict access using the .get() to prevent key error

--- hn_submissions.py
+++ hn_submissions.py
@@ -10,25 +10,27 @@ print(f"Status code: {r.status_code}")
 # Process information about each submission.
 submission_ids = r.json()
 submission_dicts = []
+
 for submission_id in submission_ids[:30]:
      
     # Make a new API call for each submission.
      url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
      r = requests.get(url)
      print(f"id: {submission_id}\tstatus: {r.status_code}")
+
      response_dict = r.json()
 
     # Build a dictionary for each article.
      submission_dict = {
-        'title': response_dict['title'],
+        'title': response_dict.get('title', 'No title'), #uses get() to avoid KeyError
         'hn_link': f"https://news.ycombinator.com/item?id={submission_id}",
-        'comments': response_dict['descendants'],
+        'comments': response_dict.get('descendants', 0), #use 0 if no comments
     }
-     submission_dicts.append(submission_dict)
+     submission_dicts.append(submission_dict) #add to list
 
-submission_dicts = sorted(submission_dicts, key=itemgetter('comments'),
+submission_dicts = sorted(submission_dicts, key=itemgetter('comments'), #sort by comments
                             reverse=True)
-
+# Print the titles and comments of the articles.
 for submission_dict in submission_dicts:
     print(f"\nTitle: {submission_dict['title']}")
     print(f"Discussion link: {submission_dict['hn_link']}")
